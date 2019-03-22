e877e4b301655820c4f9cc327691f39bbf8100ae
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-03-22 19:53:09 -0400

    🎉  initial_commit

---
+++ lib/configex.ex
@@ -0,0 +1,18 @@
+defmodule Configex do
+  @moduledoc """
+  Documentation for Configex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Configex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/configex_test.exs
@@ -0,0 +1,8 @@
+defmodule ConfigexTest do
+  use ExUnit.Case
+  doctest Configex
+
+  test "greets the world" do
+    assert Configex.hello() == :world
+  end
+end
