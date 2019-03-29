15be4d446cc51f76cb2c8e884639a4c13334beac
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-03-29 19:42:36 -0400

    ✨  Raise error is config is used on compilation time with system env

--- lib/configex.ex
+++ lib/configex.ex
@@ -3,46 +3,82 @@ defmodule Configex do
   Configex helps you to deal with Elixir configuration.
   """
 
-  def get_env!(app, key, default \\ {:no_default}) do
-    case get_env(app, key, default) do
-      {:ok, result} -> result
-      {:error, reason} -> raise(ArgumentError, reason)
+  defmodule ConfigError do
+    defexception [:message]
+  end
+
+  defmacro __using__(_opts) do
+    quote do
+      import Configex,
+        only: [
+          get_config: 2,
+          get_config: 3,
+          get_config!: 2,
+          get_config!: 3
+        ]
     end
   end
 
-  def get_env(app, key, default \\ {:no_default}) do
-    case Application.get_env(app, key) do
-      {:system, env, default} -> get_system_env(env, default)
-      {:system, env} -> get_system_env(env, default)
-      nil -> get_application_env(app, key, default)
-      value -> {:ok, value}
+  defmacro get_config(app, key) when is_atom(app) and is_atom(key) do
+    quote do
+      Configex.get_config(unquote(app), unquote(key), :no_default)
     end
   end
 
-  defp get_system_env(env, default) do
-    case {System.get_env(env), default} do
-      {nil, {:no_default}} -> {:error, "Missing ENV variable: '#{env}'"}
-      {nil, default} -> {:ok, default}
-      {value, _default} -> {:ok, value}
+  defmacro get_config(app, key, default) when is_atom(app) and is_atom(key) do
+    quote do
+      case Application.get_env(unquote(app), unquote(key), unquote(default)) do
+        {:system, env, default} -> Configex.get_system_env(env, default)
+        {:system, env} -> Configex.get_system_env(env, unquote(default))
+        :no_default -> Configex.missing_config_error(unquote(app), unquote(key))
+        value -> {:ok, value}
+      end
     end
   end
 
-  defp get_application_env(app, key, default) do
-    found =
-      app
-      |> Application.get_all_env()
-      |> Keyword.keys()
-      |> Enum.any?(&(&1 == key))
-
-    case {found, default} do
-      {false, {:no_default}} ->
-        {:error, "Missing configuration: 'config :#{app}, #{key}: <value>'"}
-
-      {false, default} ->
-        {:ok, default}
-
-      {true, _default} ->
-        {:ok, Application.get_env(app, key)}
+  defmacro get_config!(app, key) when is_atom(app) and is_atom(key) do
+    quote do
+      Configex.get_config!(unquote(app), unquote(key), :no_default)
     end
+  end
+
+  defmacro get_config!(app, key, default) when is_atom(app) and is_atom(key) do
+    quote do
+      case Configex.get_config(unquote(app), unquote(key), unquote(default)) do
+        {:ok, result} -> result
+        {:error, reason} -> raise(ConfigError, reason)
+      end
+    end
+  end
+
+  defmacro get_system_env(env, default) do
+    quote do
+      unless function_exported?(__MODULE__, :__info__, 1) do
+        Configex.env_compilation_error(unquote(env))
+      end
+
+      case System.get_env(unquote(env)) do
+        nil ->
+          case unquote(default) do
+            :no_default -> Configex.missing_env_var_error(unquote(env))
+            default -> {:ok, default}
+          end
+
+        value ->
+          {:ok, value}
+      end
+    end
+  end
+
+  def missing_config_error(app, key) do
+    {:error, "Missing configuration: 'config :#{app}, #{key}: <value>'"}
+  end
+
+  def missing_env_var_error(env) do
+    {:error, "Missing ENV variable: '#{env}'"}
+  end
+
+  def env_compilation_error(env) do
+    {:error, "ENV must not be used on compilation time: '#{env}'"}
   end
 end
