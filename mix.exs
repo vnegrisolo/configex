c1ef70b8f9a61854ee7ff9f6806acb351e5273e2
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2019-04-02 11:32:05 -0400

    ⬆️  Next version after a release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Configex.MixProject do
   def project do
     [
       app: :configex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
